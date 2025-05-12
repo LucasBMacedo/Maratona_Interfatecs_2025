d0cec380977262ccce2c76a400f3d32932327d3d
lucasbezerrademacedo <lucasbezerrademacedo@192.168.0.21>
2025-05-12 12:32:10 -0300

    Quarto commit.

--- src/fonteRevisada.java
+++ src/fonteRevisada.java
@@ -1,5 +1,24 @@
-
+import java.util.Scanner;
 
 public class fonteRevisada {
-
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		int v = sc.nextInt();
+		int c = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int tempoNoVale = (c * t * 2) + (m * t * 4);
+		int tempoNaColina = (v * t * 2) + (m * t * 2);
+		int tempoNaMontanha = (v * t * 4) + (c * t * 2);
+		
+		int menorTempoTotal = Math.min(tempoNoVale, (Math.min(tempoNaColina, tempoNaMontanha)));
+		
+		System.out.print(menorTempoTotal);
+		
+		sc.close();
+}
 }
