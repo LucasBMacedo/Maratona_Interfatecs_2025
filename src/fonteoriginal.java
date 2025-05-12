996a94e9d092df7da3ccdcb5cff0af434fcf30c1
lucasbezerrademacedo <lucasbezerrademacedo@192.168.0.21>
2025-05-12 12:03:05 -0300

    Primeiro commit.

---
+++ src/fonteoriginal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class fonteoriginal {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		int v = sc.nextInt();
+		int c = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if (v > c && v > m) {
+			System.out.print((c * t * 2 ) + (m * t * 4));
+		}
+			else if (c >= v && c >= m || v == m || v == c && v == m) {
+				System.out.println((v * t * 2 ) + (m * t * 2));
+			}
+			else if (m > c && m > v) {
+				System.out.print((v * t * 4) + (c * t * 2));	
+			}
+		sc.close();
+		}
+}
